9f0f6d0ec5a0d3c0d3b65ca3cb616ad08f2e43bb
Your Name <you@example.com>
2025-08-22 11:48:44 -0300

    Se puede pasar de pagina al panel

--- Front-End/Pantalladeinicio/JSPANTALLADEINICIO.js
+++ Front-End/Pantalladeinicio/JSPANTALLADEINICIO.js
@@ -1,8 +1,12 @@
-const button = document.getElementById("Medicion")
+const button1 = document.getElementById("Medicion")
+const button2 = document.getElementById("Panel")
 
-console.log(button)
+console.log(button1)
 
-button.addEventListener("click", function cambiarpagina(){
+button1.addEventListener("click", function cambiarpagina(){
   window.location.href = "../Pantalladegrafico/HTMLFRONT.html";
+})
+button2.addEventListener("click", function cambiarpagina(){
+  window.location.href = "../Paneldecontrol/panel.html";
 })
 
