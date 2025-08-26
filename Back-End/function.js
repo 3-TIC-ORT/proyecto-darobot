0db2d41dc8fb51f47527fdc10b571b39549d4782
Your Name <you@example.com>
2025-08-26 14:27:32 -0300

    creacion de funciones

---
+++ Back-End/function.js
@@ -0,0 +1,27 @@
+function temperatura (pedirtemperatura){ 
+console.log("temperatura")
+}
+function luz (pedirluminosidaad){
+    console.log("luminosidad")
+}
+function sonido (pedirsonido){
+    console.log("sonido")
+}
+function presion (pedirpresion){
+    console.log("presion")
+}
+function humedad (pedirhumedad){
+    console.log("humedad")
+}
+function delante (direcciondelante){
+    console.log("delante")
+}
+function atras (direccionatras){
+    console.log("atras")
+}
+function derecha (direccionderecha){
+    console.log("derecha")
+}
+function izquierda (direccionizquierda){
+    console.log("izquierda")
+}
