53d74fd1e1dbc6dd4113bc2c8c91a6acd488f6fc
Ezequiel Zwiebel <you@example.com>
2025-08-26 13:50:45 -0300

    Todas las pantallas estan conectadas

---
+++ Front-End/Paneldecontrol/panel.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("volver")
+
+console.log(button)
+
+button.addEventListener("click", function cambiarpagina(){
+  window.location.href = "../Pantalladeinicio/HTMLPANTALLADEINICIO.html";
+})
