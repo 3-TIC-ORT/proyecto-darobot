6f776f45413fe9ff39c2aed875b090368a10e12d
Ezequiel Zwiebel <you@example.com>
2025-09-17 15:51:23 -0300

    prepare maso los graficios

--- Front-End/Pantalladegrafico/JSFRONT.js
+++ Front-End/Pantalladegrafico/JSFRONT.js
@@ -11,74 +11,96 @@ if (selector.value === "Temperatura"){
 
 }
 
+let temperaturas = [
+    { label: "Lunes", y: 22 },
+    { label: "Martes", y: 25 },
+    { label: "Miércoles", y: 28 },
+    { label: "Jueves", y: 26 },
+    { label: "Viernes", y: 24 }
+]
+
+let luz = [
+    { label: "Mañana", y: 300 },
+    { label: "Tarde", y: 600 },
+    { label: "Noche", y: 150 }
+]
+
+let humedad = [
+    { label: "Enero", y: 70 },
+    { label: "Febrero", y: 65 },
+    { label: "Marzo", y: 80 },
+    { label: "Abril", y: 75 }
+]
+
+let presion = [
+    { label: "Altura 0m", y: 1013 },
+    { label: "500m", y: 954 },
+    { label: "1000m", y: 898 },
+    { label: "1500m", y: 845 }
+]
+
+let sonido = [
+    { label: "Ambiente", y: 40 },
+    { label: "Tráfico", y: 35 },
+    { label: "Máquinas", y: 25 }
+]
+
 window.onload = function () {
   const select = document.getElementById("selectordeunidades");
   const container = document.getElementById("chartContainer");
   let chart;
+  const socket = io("http://localhost:3000");
 
   select.addEventListener("change", function () {
       const opcion = select.value;
-
       if (opcion === "vacio") {
           container.innerHTML = "";
           return;
       }
-
       container.innerHTML = "";
-
       chart = new CanvasJS.Chart("chartContainer", {
           animationEnabled: true,
           theme: "light2",
-          title: { text: "Datos del " + opcion },
+          title: { text: "Datos de " + opcion },
           data: []
       });
 
       if (opcion === "Temperatura") {
-          chart.options.data = [{
-              type: "line",
-              dataPoints: [
-                  { label: "Lunes", y: 22 },
-                  { label: "Martes", y: 25 },
-                  { label: "Miércoles", y: 28 },
-                  { label: "Jueves", y: 26 },
-                  { label: "Viernes", y: 24 }
-              ]
-          }];
+          chart.options.data = [{ type: "line", dataPoints: temperaturas }];
+          socket.on("sensorDataTemperatura", (obj) => {
+              chart.options.data[0].dataPoints.push(obj);
+              if (chart.options.data[0].dataPoints.length > 20) {
+                  chart.options.data[0].dataPoints.shift();
+              }
+              chart.render();
+          });
       }
 
       if (opcion === "Luz") {
-          chart.options.data = [{
-              type: "column",
-              dataPoints: [
-                  { label: "Mañana", y: 300 },
-                  { label: "Tarde", y: 600 },
-                  { label: "Noche", y: 150 }
-              ]
-          }];
+          chart.options.data = [{ type: "column", dataPoints: luz }];
+          socket.on("sensorDataLuz", (obj) => {
+              chart.options.data[0].dataPoints.push(obj);
+              chart.render();
+          });
       }
 
       if (opcion === "Humedad") {
-          chart.options.data = [{
-              type: "line",
-              dataPoints: [
-                  { label: "Enero", y: 70 },
-                  { label: "Febrero", y: 65 },
-                  { label: "Marzo", y: 80 },
-                  { label: "Abril", y: 75 }
-              ]
-          }];
+          chart.options.data = [{ type: "line", dataPoints: humedad }];
+          socket.on("sensorDataHumedad", (obj) => {
+              chart.options.data[0].dataPoints.push(obj);
+              if (chart.options.data[0].dataPoints.length > 20) {
+                  chart.options.data[0].dataPoints.shift();
+              }
+              chart.render();
+          });
       }
 
       if (opcion === "Presion") {
-          chart.options.data = [{
-              type: "area",
-              dataPoints: [
-                  { label: "Altura 0m", y: 1013 },
-                  { label: "500m", y: 954 },
-                  { label: "1000m", y: 898 },
-                  { label: "1500m", y: 845 }
-              ]
-          }];
+          chart.options.data = [{ type: "area", dataPoints: presion }];
+          socket.on("sensorDataPresion", (obj) => {
+              chart.options.data[0].dataPoints.push(obj);
+              chart.render();
+          });
       }
 
       if (opcion === "Sonido") {
@@ -86,12 +108,12 @@ window.onload = function () {
               type: "pie",
               showInLegend: true,
               legendText: "{label}",
-              dataPoints: [
-                  { label: "Ambiente", y: 40 },
-                  { label: "Tráfico", y: 35 },
-                  { label: "Máquinas", y: 25 }
-              ]
+              dataPoints: sonido
           }];
+          socket.on("sensorDataSonido", (obj) => {
+              chart.options.data[0].dataPoints.push(obj);
+              chart.render();
+          });
       }
 
       chart.render();
