4b80e987fe172551c64b9e1ed9c2a8450e45e997
Your Name <you@example.com>
2025-09-17 10:27:18 -0300

    import serial y soquetic

--- Back-End/function.js
+++ Back-End/function.js
@@ -1,27 +1,12 @@
-function temperatura (pedirtemperatura){ 
-console.log("temperatura")
-}
-function luz (pedirluminosidaad){
-    console.log("luminosidad")
-}
-function sonido (pedirsonido){
-    console.log("sonido")
-}
-function presion (pedirpresion){
-    console.log("presion")
-}
-function humedad (pedirhumedad){
-    console.log("humedad")
-}
-function delante (direcciondelante){
-    console.log("delante")
-}
-function atras (direccionatras){
-    console.log("atras")
-}
-function derecha (direccionderecha){
-    console.log("derecha")
-}
-function izquierda (direccionizquierda){
-    console.log("izquierda")
-}
+import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
+import { SerialPort } from "serialport";
+
+let port = new SerialPort({
+    path: "COM3", 
+    baudRate: 9600, // Debe coincidir con el dispositivo
+  });
+
+  function puerto(){
+    port.on = console.log("datos recibidos")
+  }
+  
