24c9aa40038bb8eba732695047bb9286590fbe5e
Ezequiel Zwiebel <you@example.com>
2025-09-30 14:30:45 -0300

    Panel de control falta diseño

--- Front-End/Paneldecontrol/panel.js
+++ Front-End/Paneldecontrol/panel.js
@@ -1,7 +1,22 @@
-const button = document.getElementById("volver")
-
-console.log(button)
-
-button.addEventListener("click", function cambiarpagina(){
+// Botón volver
+const button = document.getElementById("volver");
+button.addEventListener("click", () => {
   window.location.href = "../Pantalladeinicio/HTMLPANTALLADEINICIO.html";
-})
+});
+
+// Control WASD
+const keys = document.querySelectorAll('.key');
+
+window.addEventListener('keydown', (e) => {
+  const key = e.key.toLowerCase();
+  keys.forEach(k => {
+    if (k.dataset.key === key) k.classList.add('active');
+  });
+});
+
+window.addEventListener('keyup', (e) => {
+  const key = e.key.toLowerCase();
+  keys.forEach(k => {
+    if (k.dataset.key === key) k.classList.remove('active');
+  });
+});
