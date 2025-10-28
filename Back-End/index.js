0368b0154144743fbc22ae2a119e73d855eceb6b
Your Name <you@example.com>
2025-10-28 14:25:52 -0300

    booooooca

--- Back-End/index.js
+++ Back-End/index.js
@@ -3,7 +3,7 @@ import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } fro
 
 // Create a port
 const port = new SerialPort({
-  path: 'COM3',
+  path: 'COM5',
   baudRate: 9600,
 })
  
@@ -11,12 +11,30 @@ port.on("open",()=>{
     console.log("Hola")
     //Borrar el setInterval-Lo unico importante es la funcion
 })
+
 //Conexion con Hardware
 function enviarAArduino(mandar){
     port.write(mandar.toString() + "\n")
 } // Enviar info al arduino
-
-subscribeGETEvent("medir", enviarAArduino(1))
+ function medicion(Tipo){
+  if (Tipo === "humedad") {
+    enviarAArduino("MH")
+  };
+  if (Tipo === "temperatura") {
+    enviarAArduino("MT")
+  };
+  if (Tipo === "presion") {
+    enviarAArduino("MP")
+  };
+  if (Tipo === "luz") {
+    enviarAArduino("ML")
+  };
+  if (Tipo === "sonido") {
+    enviarAArduino("MS")
+  };
+ 
+ };
+subscribePOSTEvent("medir", medicion)
 
 const lectura = port.pipe(new ReadlineParser({ delimiter: '\r\n' })) //Leer info de arduino
 lectura.on('data', (data) => console.log(data));
